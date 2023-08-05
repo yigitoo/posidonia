21614fda94269ec7783628a02cb7fdeba17a6cc3
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-08-05 20:50:31 +0300

    Change scripts/desktop_app.py Line:5

--- scripts/desktop_app.py
+++ scripts/desktop_app.py
@@ -2,7 +2,7 @@ import webview, os
 
 os.environ['APP_URL'] = 'http://localhost:1234/'
 
-link_of_webview = input("Give the link of app:")
+link_of_webview = input("Give the URL link of app: ")
 if link_of_webview not in ['', None]:
     os.environ['APP_URL'] = link_of_webview
 
